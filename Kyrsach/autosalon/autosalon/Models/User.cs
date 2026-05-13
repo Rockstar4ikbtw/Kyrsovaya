54de496cc449163b12c9f37a1803369a7cfabace
aeqweqweqwe <yaroslaff.b@gmail.com>
2026-05-13 22:13:27 +0300

    Update User.cs

--- Kyrsach/autosalon/autosalon/Models/User.cs
+++ Kyrsach/autosalon/autosalon/Models/User.cs
@@ -1,6 +1,29 @@
-﻿namespace autosalon.Models
+using Microsoft.AspNetCore.Identity.Data;
+
+namespace autosalon.Models
 {
+    public enum Role
+    {
+        User = 1,
+        Admin = 2,
+        Manager = 3,
+        Accountant = 4
+    }
+
     public class User
     {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public string Phone { get; set; }
+
+        public string Email { get; set; }
+
+        public ICollection<Sale> Sales { get; set; }
     }
 }
