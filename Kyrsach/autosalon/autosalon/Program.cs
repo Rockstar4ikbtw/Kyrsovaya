5a6499acce8345d961ba62776caaa24ebad7433c
aeqweqweqwe <yaroslaff.b@gmail.com>
2026-05-13 22:47:20 +0300

    Update Program.cs

--- Kyrsach/autosalon/autosalon/Program.cs
+++ Kyrsach/autosalon/autosalon/Program.cs
@@ -1,11 +1,16 @@
-var builder = WebApplication.CreateBuilder(args);
+using autosalon.Data;
+using Microsoft.EntityFrameworkCore;
 
-// Add services to the container.
+
+var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 var app = builder.Build();
 
