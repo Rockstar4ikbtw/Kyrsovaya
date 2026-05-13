5efd1ec76adcdf266c10edb5b22f7c67d0fc92e1
aeqweqweqwe <yaroslaff.b@gmail.com>
2026-05-13 23:24:44 +0300

    Update UsersController.cs

--- Kyrsach/autosalon/autosalon/Controllers/UsersController.cs
+++ Kyrsach/autosalon/autosalon/Controllers/UsersController.cs
@@ -1,6 +1,78 @@
-﻿namespace autosalon.Controllers
+using autosalon.Data;
+using autosalon.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using autosalon.Data;
+using autosalon.Models;
+
+namespace YourProject.Controllers
 {
-    public class UsersController
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
     {
+        private readonly AppDbContext _db;
+
+        public UsersController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var users = await _db.Users.ToListAsync();
+
+            return Ok(users);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(int id)
+        {
+            var user = await _db.Users.FindAsync(id);
+
+            if (user == null)
+                return NotFound();
+
+            return Ok(user);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] User user)
+        {
+            _db.Users.Add(user);
+
+            await _db.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Update(int id, [FromBody] User updated)
+        {
+            var user = await _db.Users.FindAsync(id);
+
+            if (user == null)
+                return NotFound();
+
+            await _db.SaveChangesAsync();
+
+            return Ok(user);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var user = await _db.Users.FindAsync(id);
+
+            if (user == null)
+                return NotFound();
+
+            _db.Users.Remove(user);
+
+            await _db.SaveChangesAsync();
+
+            return NoContent();
+        }
     }
 }
