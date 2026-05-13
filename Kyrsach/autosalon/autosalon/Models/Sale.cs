8494704f649174f8aca5d2ee21d69e310fa9c312
aeqweqweqwe <yaroslaff.b@gmail.com>
2026-05-13 22:15:40 +0300

    Update Sale.cs

--- Kyrsach/autosalon/autosalon/Models/Sale.cs
+++ Kyrsach/autosalon/autosalon/Models/Sale.cs
@@ -1,6 +1,41 @@
-﻿namespace autosalon.Models
+using System.ComponentModel.DataAnnotations;
+
+namespace autosalon.Models
 {
     public class Sale
     {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Марка автомобиля обязательна!")]
+        [StringLength(100, ErrorMessage = "Марка не должна быть длиннее 100 символов!")]
+        public string Brand { get; set; }
+
+        [Required(ErrorMessage = "Дата обязательна!")]
+        public DateTime Date { get; set; }
+
+        [Range(0, 100000000, ErrorMessage = "Цена указана неверно!")]
+        public int Price { get; set; }
+
+        [Required]
+        public int ClientId { get; set; }
+
+        public User? Client { get; set; }
+
+
+        [Required]
+        public int CarId { get; set; }
+
+        public Car? Car { get; set; }
+
+
+        [Required]
+        public int ManagerId { get; set; }
+
+        public User? Manager { get; set; }
+
+
+        public int? ApplicationId { get; set; }
+
+        public Application? Application { get; set; }
     }
 }
