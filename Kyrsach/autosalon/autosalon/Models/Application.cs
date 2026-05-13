fd2b8c335130fa0b936edbb26b2aeaea6b5546f9
aeqweqweqwe <yaroslaff.b@gmail.com>
2026-05-13 22:16:47 +0300

    Update Application.cs

--- Kyrsach/autosalon/autosalon/Models/Application.cs
+++ Kyrsach/autosalon/autosalon/Models/Application.cs
@@ -1,6 +1,24 @@
-﻿namespace autosalon.Models
+using System.ComponentModel.DataAnnotations;
+
+namespace autosalon.Models
 {
+    public enum Statuse
+    {
+        New = 1,
+        InProgress = 2,
+        Approved = 3,
+        Rejected = 4
+    }
+
     public class Application
     {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Дата обязательна!")]
+        public DateTime DateTime { get; set; }
+
+        public int? SaleId { get; set; }
+
+        public Sale? Sale { get; set; }
     }
 }
