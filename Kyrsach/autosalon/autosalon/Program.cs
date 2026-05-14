96266a0599b7baa6579eb1c602aa88c02e71f396
aeqweqweqwe <yaroslaff.b@gmail.com>
2026-05-15 01:48:50 +0300

    Update Program.cs

--- Kyrsach/autosalon/autosalon/Program.cs
+++ Kyrsach/autosalon/autosalon/Program.cs
@@ -2,11 +2,13 @@ using autosalon.Data;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddControllers();
-
+builder.Services.AddControllers()
+    .AddJsonOptions(options =>
+    {
+        options.JsonSerializerOptions.ReferenceHandler =
+            System.Text.Json.Serialization.ReferenceHandler.IgnoreCycles;
+    });
 builder.Services.AddEndpointsApiExplorer();
-
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddDbContext<AppDbContext>(options =>
@@ -16,22 +18,25 @@ builder.Services.AddDbContext<AppDbContext>(options =>
 );
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowAll", policy =>
+    options.AddPolicy("frontend", policy =>
     {
-        policy.AllowAnyOrigin()
-              .AllowAnyHeader()
-              .AllowAnyMethod();
+        policy
+    .WithOrigins(
+
+        "http://127.0.0.1:5500",
+        "http://localhost:5500"
+    )
+    .AllowAnyHeader()
+    .AllowAnyMethod();
     });
 });
-
 var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-app.UseHttpsRedirection();
-app.UseCors("AllowAll");
+app.UseCors("frontend");
 app.UseAuthorization();
 app.MapControllers();
 app.Run();
