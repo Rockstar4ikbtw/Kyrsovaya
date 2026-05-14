be662c6c779d8de770a6388f9290373608c05374
aeqweqweqwe <yaroslaff.b@gmail.com>
2026-05-15 01:54:06 +0300

    Update CarsController.cs

--- Kyrsach/autosalon/autosalon/Controllers/CarsController.cs
+++ Kyrsach/autosalon/autosalon/Controllers/CarsController.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore;
 using autosalon.Data;
 using autosalon.Models;
 
-namespace YourProject.Controllers
+namespace autosalon.Controllers
 {
     [ApiController]
     [Route("api/[controller]")]
