2dca195bae14e9585dc5cbe590eeb0bdefa5d706
Rockstar4ikbtw <yaroslaff.b@gmail.com>
2026-05-19 10:11:11 +0300

    fix: remove trailing slashes from CORS origins

--- autosalon/Program.cs
+++ autosalon/Program.cs
@@ -22,11 +22,10 @@ builder.Services.AddCors(options =>
     {
         policy
     .WithOrigins(
-
-        "http://127.0.0.1:5500/",
-        "http://localhost:5500/",
-        "http://localhost:3000/",
-        "http://127.0.0.1:3000/"
+        "http://127.0.0.1:5500",
+        "http://localhost:5500",
+        "http://localhost:3000",
+        "http://127.0.0.1:3000"
     )
     .AllowAnyHeader()
     .AllowAnyMethod();
